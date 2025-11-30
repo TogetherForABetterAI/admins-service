7a9da8a1549cc44c3fdaa61b657ce8ac5697b72c
iinaki <llorensinaki22@gmail.com>
2025-11-30 13:52:56 -0300

    fix: Error en pnpm build y NextJS error

--- src/app/set-password/page.tsx
+++ src/app/set-password/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 import { SetPasswordCard } from "@/components/set-password-card";
-import { useState } from "react";
+import { Suspense } from "react";
 
 
 export default function SetPasswordPage() {
     return (
         <div className="flex min-h-screen flex-col items-center justify-center bg-neutral-50 p-4">
-            <SetPasswordCard />
+            <Suspense fallback={<div>Loading...</div>}>
+                <SetPasswordCard />
+            </Suspense>
         </div>
     );
 }
