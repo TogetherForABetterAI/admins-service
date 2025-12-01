1b418b29eddf7ff7deea30553f170485f52606ec
tinchomorilla <mmorilla@fi.uba.ar>
2025-12-01 17:30:22 -0300

    remove output option from next.config.ts for Vercel compatibility

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
+  // output: "standalone", // Solo para Docker, no para Vercel
   };
 
 export default nextConfig;
